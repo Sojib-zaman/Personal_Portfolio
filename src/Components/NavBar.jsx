91ba67954ddaf2e5b5afcf01f271590b00175a20
Sojib-zaman <sojibxaman439@gmail.com>
2024-08-08 23:24:42 +0600

    removed navbar issues

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -12,9 +12,19 @@ const NavBar = () => {
     { id: 6, link: "ContactMe" },
 
   ];
+  var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
 
   return (
-    <div className="flex justify-between items-center h-20 py-3 px-5 w-full text-white bg-transparent fixed">
+    <div className="flex justify-between items-center h-20 py-3 px-5 w-full text-white bg-transparent fixed" id="navbar">
 
 
       <div>
@@ -25,9 +35,10 @@ const NavBar = () => {
           otherpages.map((page) => (
             <a href={`#${page.link}`}className="text-gray-500 cursor-pointer px-4 hover:text-sky-400 hover:scale-105 duration-150">{page.link}</a>
           )
-          )}
+          )
+        }
       </ul>
-
+      
 
       </div>
   )
